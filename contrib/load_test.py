89f8fc11da55fee94d2f9460214d2feab4293b0a
Claudio Magalhães <cjasm@cin.ufpe.br>
2022-08-27 10:47:45 -0300

    Que tal tentar salvar alguma coisa

--- contrib/load_test.py
+++ contrib/load_test.py
@@ -1,9 +1,11 @@
 from locust import HttpUser, between, task
 import logging
+import faker
 
 
 class MyFoodUser(HttpUser):
     wait_time = between(1, 5)
+    faker = faker.Faker()
 
     def on_start(self):
         logging.info("Estou iniciando o meu usuário")
@@ -11,6 +13,20 @@ class MyFoodUser(HttpUser):
     def on_stop(self):
         logging.info("Estou encerrando o meu usuário")
 
-    @task
+    @task(3)
     def list_meals(self):
         self.client.get('/api/meals/')
+
+    @task
+    def create_meal(self):
+        meal_name = self.faker.bothify(text='???####')
+        data = {
+            "sku": meal_name,
+            "slug": meal_name,
+            "title": "Grandma Soup",
+            "description": "My Favorite Grandma Soup",
+            "category": "mains",
+            "start_date": "2022-07-31",
+            "end_date": "2030-07-31"
+        }
+        self.client.post('/api/meals/', json=data)
