c50e02b0560a06ccf7780f03c2952cd20b7eebed
Claudio Magalhães <cjasm@cin.ufpe.br>
2022-10-12 19:38:11 -0300

    add invokust

---
+++ contrib/invokust_conf.py
@@ -0,0 +1,13 @@
+import invokust
+
+settings = invokust.create_settings(
+    locustfile='./contrib/load_test.py',
+    host='http://aguentaotranco.pythonanywhere.com/api/',
+    num_users=10,
+    spawn_rate=1,
+    run_time='1m'
+)
+
+loadtest = invokust.LocustLoadTest(settings)
+loadtest.run()
+loadtest.stats()
